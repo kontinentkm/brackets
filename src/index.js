c435069742f70e7f2552046f4ba2cd794e4e9bb9
Max <KontinentKM@gmail.com>
2023-02-26 21:10:17 +0300

    feat: add basic page layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	const BRACKETS_PAIR = new Map(bracketsConfig);
+	let stack = [];
+	for (let i = 0; i < str.length; i++) {
+		let currentSymbol = str[i];
+		let topElement = stack[stack.length - 1];
+
+		if (BRACKETS_PAIR.get(topElement) === currentSymbol) {
+			stack.pop();
+		} else {
+			stack.push(currentSymbol)
+		}
+	}
+
+	return stack.length === 0;
 }
